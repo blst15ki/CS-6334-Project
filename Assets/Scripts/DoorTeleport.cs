12f787403d512987fc26110102ca584ee29dde03
blst15ki <bldsad888@gmail.com>
2024-03-30 13:04:56 -0500

    Add teleport to outside scene

---
+++ Assets/Scripts/DoorTeleport.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DoorTeleport : MonoBehaviour
+{
+    [SerializeField] string scene;
+    [SerializeField] GameObject sign;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sign.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // if door collide with player, teleport inside
+        // SceneManager.LoadScene(scene);
+    }
+
+    public void PointerOn() { sign.SetActive(true); }
+    public void PointerOff() { sign.SetActive(false); }
+
+    void OnTriggerEnter(Collider collider) {
+        if(collider.gameObject.name == "Character") {
+            SceneManager.LoadScene(scene);
+        }
+    }
+}
