9cae8f70bec1f6402465dfe8dcd5370805142c97
Milind Choudhary <milindc02@gmail.com>
2024-04-30 21:40:16 -0500

    Revert "Added Player Num"
    This reverts commit f9cae802318e3e273bb2a15eb58bca6e48f0df51.

--- Assets/Scripts/PlayerSetup.cs
+++ Assets/Scripts/PlayerSetup.cs
@@ -2,14 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
-using UnityEngine.UI;
-using TMPro;
+
 public class PlayerSetup : MonoBehaviourPun
 {
     public CharacterMovement movement;
     public GameObject camera;
-    private int playerNum;
-    [SerializeField] TMP_Text playerText;
+
     void Start() {
         if (photonView.IsMine) {
             EnablePlayer();
@@ -22,9 +20,6 @@ public class PlayerSetup : MonoBehaviourPun
     void EnablePlayer() {
         movement.enabled = true;
         camera.SetActive(true);
-        System.Random rnd = new System.Random();
-        playerNum  = rnd.Next(1, 10);
-        playerText.text = "Player " + playerNum.ToString();
     }
 
     void DisablePlayer() {
