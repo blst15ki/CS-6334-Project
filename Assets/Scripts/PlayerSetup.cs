f9cae802318e3e273bb2a15eb58bca6e48f0df51
Milind Choudhary <milindc02@gmail.com>
2024-04-30 21:00:30 -0500

    Added Player Num
    - Added player num for better look in the lobby.

--- Assets/Scripts/PlayerSetup.cs
+++ Assets/Scripts/PlayerSetup.cs
@@ -2,12 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
-
+using UnityEngine.UI;
+using TMPro;
 public class PlayerSetup : MonoBehaviourPun
 {
     public CharacterMovement movement;
     public GameObject camera;
-
+    private int playerNum;
+    [SerializeField] TMP_Text playerText;
     void Start() {
         if (photonView.IsMine) {
             EnablePlayer();
@@ -20,6 +22,9 @@ public class PlayerSetup : MonoBehaviourPun
     void EnablePlayer() {
         movement.enabled = true;
         camera.SetActive(true);
+        System.Random rnd = new System.Random();
+        playerNum  = rnd.Next(1, 10);
+        playerText.text = "Player " + playerNum.ToString();
     }
 
     void DisablePlayer() {
