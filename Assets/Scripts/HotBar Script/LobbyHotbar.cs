aa803f3fa3b77e4c77157c7f1694282c9b4478e9
Milind Choudhary <milindc02@gmail.com>
2024-05-02 14:38:47 -0500

    Revert "fix: test"
    This reverts commit 860b51096d99308132014532f940ee580c001af1.

--- Assets/Scripts/HotBar Script/LobbyHotbar.cs
+++ Assets/Scripts/HotBar Script/LobbyHotbar.cs
@@ -215,19 +215,19 @@ public class LobbyHotbar : Hotbar
 				case "None":
 					break;
 				case "Watering Can":
-					obj = PhotonNetwork.InstantiateRoomObject("WateringCanPrefab", listOfHotBarItem[i].position, Quaternion.identity);
+					obj = PhotonNetwork.Instantiate("WateringCanPrefab", listOfHotBarItem[i].position, Quaternion.identity);
 					SelectObject(obj, i);
 					break;
 				case "Sprinkler":
-					obj = PhotonNetwork.InstantiateRoomObject("Sprinkler", listOfHotBarItem[i].position, Quaternion.identity);
+					obj = PhotonNetwork.Instantiate("Sprinkler", listOfHotBarItem[i].position, Quaternion.identity);
 					SelectObject(obj, i);
 					break;
 				case "Pot":
-					obj = PhotonNetwork.InstantiateRoomObject("flowerpot", listOfHotBarItem[i].position, Quaternion.identity);
+					obj = PhotonNetwork.Instantiate("flowerpot", listOfHotBarItem[i].position, Quaternion.identity);
 					Pot pot = obj.GetComponent<Pot>();
 
 					if(listOfHotBarItem[i].hasPlant) {
-						GameObject objPlant = PhotonNetwork.InstantiateRoomObject(listOfHotBarItem[i].plantType, listOfHotBarItem[i].plantPosition, Quaternion.identity);
+						GameObject objPlant = PhotonNetwork.Instantiate(listOfHotBarItem[i].plantType, listOfHotBarItem[i].plantPosition, Quaternion.identity);
 						Plant networkPlant = objPlant.GetComponent<Plant>();
 						networkPlant.SetPlantType(listOfHotBarItem[i].plantType);
 						networkPlant.SetWater(listOfHotBarItem[i].water);
@@ -249,11 +249,11 @@ public class LobbyHotbar : Hotbar
 					break;
 				case "Seed":
 					string seedString = listOfHotBarItem[i].seedType + " Seed";
-					obj = PhotonNetwork.InstantiateRoomObject(seedString, listOfHotBarItem[i].position, Quaternion.identity);
+					obj = PhotonNetwork.Instantiate(seedString, listOfHotBarItem[i].position, Quaternion.identity);
 					SelectObject(obj, i);
 					break;
 				case "Fertilizer":
-					obj = PhotonNetwork.InstantiateRoomObject("Fertilizer Cube", listOfHotBarItem[i].position, Quaternion.identity);
+					obj = PhotonNetwork.Instantiate("Fertilizer Cube", listOfHotBarItem[i].position, Quaternion.identity);
 					SelectObject(obj, i);
 					break;
 				default:
